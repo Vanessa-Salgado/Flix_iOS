b8c5fa5314d0252e4d0c195078015da6b952017d
Vanessa-Salgado <vsalgadozavaleta@ucsb.edu>
2021-10-10 23:18:02 -0700

    vs

---
+++ Flixer/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flixer
+//
+//  Created by Vanessa Salgado on 10/10/21.
+//  Copyright © 2021 com.vactives12. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
